9c41be053a10009b49a9eb3128a1fb53ff52da38
Aung Myat Moe <100549746+aungmyatmoe11@users.noreply.github.com>
2024-11-09 22:06:35 +0630

    37 : Handling PATCH Request

--- route-handers/src/app/comments/[id]/route.ts
+++ route-handers/src/app/comments/[id]/route.ts
@@ -8,3 +8,17 @@ export async function GET(
 
   return Response.json(comment)
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { text } = await request.json()
+
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  )
+  comments[index].text = text
+
+  return Response.json(comments[index])
+}
