96ed1bb91c88c039d4d6f39d1f037ae92e01289d
Aung Myat Moe <100549746+aungmyatmoe11@users.noreply.github.com>
2024-11-09 22:39:03 +0630

    42 : Cookie in Route Handlers

--- route-handers/src/app/profile/api/route.ts
+++ route-handers/src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server"
-import { headers } from "next/headers"
+import { cookies, headers } from "next/headers"
 
 export async function GET(request: NextRequest) {
   const requestHandlers = new Headers(request.headers)
@@ -8,9 +8,17 @@ export async function GET(request: NextRequest) {
   console.log(requestHandlers.get("Authorization"))
   console.log(headerList.get("Authorization"))
 
+  // ! Cookie
+  const theme = request.cookies.get("theme")
+  cookies().set("resultsPerPage", "20")
+
+  console.log(theme)
+  console.log(cookies().get("resultsPerPage"))
+
   return new Response("<h1>Profile API DATA</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   })
 }
