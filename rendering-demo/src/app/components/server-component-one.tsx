f15b87503722bfba3741cd436501db8623c37eec
Aung Myat Moe <100549746+aungmyatmoe11@users.noreply.github.com>
2024-11-10 13:12:27 +0630

    61 : Interleaving Server and Client Components

--- rendering-demo/src/app/components/server-component-one.tsx
+++ rendering-demo/src/app/components/server-component-one.tsx
@@ -1,6 +1,6 @@
 import fs from "fs";
 
 export const ServerComponentOne = () => {
-  fs.readFileSync("src/components/server-component-one.tsx", "utf-8");
+  // fs.readFileSync("src/components/server-component-one.tsx", "utf-8"); // TODO : pwint htr ya mr
   return <h1>ServerComponentOne</h1>;
 };
