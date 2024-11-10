bd9958c12672f2be54b32b116d35d773124d667f
Aung Myat Moe <100549746+aungmyatmoe11@users.noreply.github.com>
2024-11-10 10:56:47 +0630

    59 : Cient Only Code

--- rendering-demo/src/app/server-route/page.tsx
+++ rendering-demo/src/app/server-route/page.tsx
@@ -1,12 +1,15 @@
 import { ImageSlider } from "../components/image-slider";
 import { serverSideFunction } from "../utils/server-utils";
+// import { clientSideFunction } from "../utils/client-util";
 
 export default function ServerRoutePage() {
   const result = serverSideFunction();
+  // const clientResult = clientSideFunction();
   return (
     <>
       <h1>Server Route</h1>
       <p>{result}</p>
+      {/* <p>{clientResult}</p> */}
       <ImageSlider />
     </>
   );
