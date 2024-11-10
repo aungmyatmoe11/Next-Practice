0edd693aba5d618110ded4032087cefde6c7e139
Aung Myat Moe <100549746+aungmyatmoe11@users.noreply.github.com>
2024-11-10 13:46:34 +0630

    63 : Fetching Data with Server

--- data-fetching-demo/src/app/users/page.tsx
+++ data-fetching-demo/src/app/users/page.tsx
@@ -7,7 +7,6 @@ type User = {
   };
   
   export default async function UsersPage() {
-    await new Promise((resolve) => setTimeout(resolve, 2000)); // ! FOR LOADING
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await response.json();
     return (
